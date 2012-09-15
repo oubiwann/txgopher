c6b53b56967d09ec1a5d10d71009ea9859d91333
Duncan McGreggor <duncan@dreamhost.com>
2012-09-15 12:25:17 -0700

    Started work on the gopher item types.

---
+++ txgopher/protocol.py
@@ -0,0 +1,57 @@
+class GopherItemTypes(object):
+    """
+    Historical notes:
+
+    A CCSO name-server or Ph protocol was an early form of database search on
+    the web. In its most common form it was used to look up information such as
+    phone numbers and e-mail addresses. Today this service has been largely
+    replaced by LDAP. It was used mainly in the early-to-middle 1990s. The
+    name-server was developed by Steve Dorner at the University of Illinois at
+    Urbana-Champaign.
+
+    BinHex, short for "binary-to-hexadecimal", is a binary-to-text encoding
+    system that was used on the Mac OS for sending binary files through e-mail.
+    It is similar to Uuencode, but combined both "forks" of the Mac file system
+    together along with extended file information. BinHexed files take up more
+    space than the original files, but will not be corrupted by non-"8-bit
+    clean" software.
+    """
+    map = {
+        "file": 0,
+        "directory": 1,
+        # see docstring
+        "ccsoNameServer": 2,
+        "error": 3,
+        # see docstring
+        "binHex": 4,
+        # DOS binary archive of some sort
+        "dosBin": 5,
+        # a UNIX uuencoded file
+        "uuencode": 6,
+        "searchServer": 7,
+        "telnetSession": 8,
+        "binaryFile": 9,
+        "redundantServer": "+",
+        "tn3270Session": "T",
+        "gif": "g",
+        "image": "I",
+
+    def __getattr__(self, name):
+        value = self.map.get(name)
+        if value:
+            return value
+        else:
+            return super(GopherItemTypes, self).__getattr__(name)
+
+    def get(self, value):
+        if value in self.map.keys():
+            return self.map[value]
+        elif value in self.map.values():
+            for key, val in self.map.items():
+                if val == value:
+                    return key
+
+
+class GopherPlusItemTypes(GopherItemTypes):
+    """
+    """
