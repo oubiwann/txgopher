9cb498e72da484297b81481ad869cf6809457f62
Duncan McGreggor <duncan@dreamhost.com>
2012-09-15 12:24:34 -0700

    Added a txGopher banner.

--- txgopher/const.py
+++ txgopher/const.py
@@ -1,2 +1,9 @@
 port = 70
 defaultURL = "gopher://gopher.floodgap.com/"
+banner = r"""
+
+ |         __|          |
+  _|\ \ / (_ |  _ \ _ \   \   -_)  _|
+\__| _\_\\___|\___/.__/_| _|\___|_|
+                  _|
+"""
