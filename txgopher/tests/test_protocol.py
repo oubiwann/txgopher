2f8fd476dfeb0843431a7dddcbfee273e4b7a70f
Duncan McGreggor <duncan@dreamhost.com>
2012-09-15 19:40:13 -0700

    Added unit tests for protocol item types.

---
+++ txgopher/tests/test_protocol.py
@@ -0,0 +1,26 @@
+from twisted.trial import unittest
+
+from txgopher import protocol
+
+
+class GopherItemTypesTestCase(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+        self.itemType = protocol.GopherItemTypes()
+
+    def test_getByKey(self):
+        self.assertEqual(self.itemType.get("file"), 0)
+        self.assertEqual(self.itemType.get("uuencode"), 6)
+        self.assertEqual(self.itemType.get("binaryFile"), 9)
+
+    def test_getByValue(self):
+        self.assertEqual(self.itemType.get(0), "file")
+        self.assertEqual(self.itemType.get(1), "directory")
+        self.assertEqual(self.itemType.get(2), "ccsoNameServer")
+
+    def test_getByAttribute(self):
+        self.assertEqual(self.itemType.file, 0)
+        self.assertEqual(self.itemType.tn3270Session, "T")
+        self.assertEqual(self.itemType.gif, "g")
+
