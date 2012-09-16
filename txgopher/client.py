ea44de6e3f5cf7ce6d90275c45e07f512bbfd46f
Duncan McGreggor <duncan@dreamhost.com>
2012-09-16 02:00:29 -0700

    Cleaned up client code.

--- txgopher/client.py
+++ txgopher/client.py
@@ -5,36 +5,46 @@ from twisted.protocols.basic import LineReceiver
 from txgopher import const, protocol
 
 
-def welcome(additionalText=""):
-    if not additionalText:
-        additionalText = "A client for gopherspace.\n"
-    separator = "-" * const.pageWidth
-    return "%s%s\n%s%s" % (
-        separator, const.banner, additionalText, separator)
-
 
 class GopherClient(LineReceiver):
     """
     """
-    def __init__(self, host, selector, debug=False):
-        self.host = host
+    def __init__(self, selector, debug=False):
         self.selector = selector
         self.debug = debug
+
+    def getBanner(self, additionalText=""):
+        if not self.factory.withBanner:
+            return
+        if not additionalText:
+            additionalText = "A client for gopherspace.\n"
+        separator = "-" * const.pageWidth
+        return "%s%s\n%s%s" % (
+            separator, const.banner, additionalText, separator)
 
     def sendCommand(self, selector=""):
         if not selector:
             selector = self.selector
         self.sendLine(selector)
 
-    def lineReceived(self, line):
-        parsed = protocol.GopherLineProtocol(self.host, line)
-        if self.debug:
+    def handleGopherMapLine(self, line):
+        parsed = protocol.GopherMapProtocol(self.factory.host, line)
+        if self.factory.debug:
             print parsed
+        elif parsed.type in const.RESOURCES:
+            print "%s <link: %s>" % (parsed.display, parsed.url)
         else:
             print parsed.display
-        if parsed.type == const.DISCONNECT:
+
+    def lineReceived(self, line):
+        if line == const.DISCONNECT:
             self.transport.loseConnection()
             reactor.stop()
+        elif self.factory.type == const.DIR:
+            self.handleGopherMapLine(line)
+        elif self.factory.type in const.FILES:
+            print line
+
 
     def connectionMade(self):
         self.sendCommand()
@@ -43,10 +53,14 @@ class GopherClient(LineReceiver):
 class GopherClientFactory(Factory):
     """
     """
-    def __init__(self, host, selector, debug=False):
+    def __init__(self, host, type, selector, debug=False, withBanner=True):
         self.host = host
+        self.type = type
         self.selector = selector
         self.debug = debug
+        self.withBanner = withBanner
 
     def buildProtocol(self, addr):
-        return GopherClient(self.host, self.selector, self.debug)
+        protocol = GopherClient(self.selector, self.debug)
+        protocol.factory = self
+        return protocol
