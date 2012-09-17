05910e877d8e5136e784e9eff98fc6cf736ede25
Duncan McGreggor <duncan@dreamhost.com>
2012-09-16 17:21:12 -0700

    Added test for searching.

--- txgopher/tests/test_utils.py
+++ txgopher/tests/test_utils.py
@@ -8,17 +8,23 @@ class UtilsTestCase(unittest.TestCase):
     """
     def test_getClientParamsNoURL(self):
         result = utils.getClientParams()
-        expected = ('1', '/', 'sdf.lonestar.org', 70)
+        expected = ('1', '/', 'sdf.lonestar.org', 70, '')
         self.assertEqual(result, expected)
 
     def test_getClientParamsWithItemType(self):
         url = "gopher://hostname:9999/0/path/to/a/file.txt"
         result = utils.getClientParams(url)
-        expected = ('0', '/path/to/a/file.txt', 'hostname', '9999')
+        expected = ('0', '/path/to/a/file.txt', 'hostname', '9999', '')
         self.assertEqual(result, expected)
 
     def test_getClientParamsWithoutItemType(self):
         url = "gopher://hostname:9999/path/to/a/file.txt"
         result = utils.getClientParams(url)
-        expected = ('1', '/path/to/a/file.txt', 'hostname', '9999')
+        expected = ('1', '/path/to/a/file.txt', 'hostname', '9999', '')
+        self.assertEqual(result, expected)
+
+    def test_getClientParamsWithSearchAndQuery(self):
+        url = "gopher://hostname:9999/7?search text"
+        result = utils.getClientParams(url)
+        expected = ('7', '/', 'hostname', '9999', 'search text')
         self.assertEqual(result, expected)
